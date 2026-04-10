996b716acccd0ff9e548227d86866542465abcad
joacorob <joaquin@raice.uy>
2026-04-10 14:50:52 +0000

    fix: strip query string from mongodb URI when deriving DB name

--- src/config/databases.js
+++ src/config/databases.js
@@ -3,6 +3,6 @@ const dbsUrls = process.env.DBS;
 export const databases = dbsUrls.split(",").map((entry) => {
   const [url, alias] = entry.split("#");
   const type = url.split(":")[0];
-  const name = alias || url.split("/")[url.split("/").length - 1];
+  const name = alias || url.split("/").pop().split("?")[0];
   return { name, url, type };
 });
